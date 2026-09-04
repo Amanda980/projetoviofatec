b6717288001a5bbba6310e81767c02ec0107d495
Amanda Cristina Olegário <amandaolegario1111@gmail.com>
2026-09-03 22:09:48 -0300

    fix(login): alterar mensagem inicial da tela

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -96,7 +96,7 @@ function Login() {
           <LockOutlinedIcon />
         </Avatar>
         <Typography component="h1" variant="h5">
-          Vio - Testando uma alteração
+          Vio - 4DSM_2026
         </Typography>
         <Box
           component="form"
